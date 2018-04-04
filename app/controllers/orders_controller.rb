d6955eb9740da378a4dd4ae68145949716decc6d
kmflucero <kevin.lucero@gmail.com>
2018-04-03 23:13:58 -0300

    orden por ajax en product

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,39 +14,27 @@ class OrdersController < ApplicationController
   def destroy
     @order = Order.find(params[:id])
     if @order.quantity==1
-      if @order.destroy!
-        redirect_to orders_path, notice: 'Carro actualizado'
-      else
-        redirect_to orders_path, notice: 'no se pudo actualizar'
-      end
+     @order.destroy!
+
     elsif @order.quantity > 1
       @order.quantity -= 1
-      if @order.save
-        redirect_to orders_path, notice: 'Carro actualizado'
-      else
-        redirect_to orders_path, notice: 'No se h podido actualizar'
-      end
+      @order.save
     end
   end
 
   def create
+    @orders = current_user.cart
     @previous_order= Order.find_by(user_id: current_user.id, product_id: params[:product_id], payed: false)
     if @previous_order.present?
- 
       new_quantity = @previous_order.quantity + 1
       @previous_order.update(quantity: new_quantity)
-      redirect_to root_path, notice: 'Se ha agregado orden con exito'
     else
       @order = Order.new
       @product = Product.find(params[:product_id])
       @order.product = @product
       @order.price =  @product.price
       @order.user = current_user
-      if @order.save
-        redirect_to root_path, notice: 'Se ha agregado orden con exito'
-      else
-        redirect_to root_path, alert: 'No se ha agregado orden con exito'
-      end
+      @order.save
     end
   end
 end
