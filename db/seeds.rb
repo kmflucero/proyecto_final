bd9611bf116301e251d8be6c19e74b7d4eccc7bf
kmflucero <kevin.lucero@gmail.com>
2018-04-08 19:58:01 -0300

    antes del theme de active admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-addresses = "289 Constitution Court
-Liverpool, NY 13090
-8248 Buttonwood Drive
-Herndon, VA 20170
-953 Lake View Lane
-Greensburg, PA 15601
-8489 Smith Store Street
-Wake Forest, NC 27587
-60 Pumpkin Hill Dr.
-South Ozone Park, NY 11420
-923 Wakehurst Lane
-New Orleans, LA 70115
-995 Lakewood Court
-Upland, CA 91784
-"
-Store.destroy_all
-addresses = addresses.split("\n")
-addresses.each do |address|
- Store.create!(name: Faker::Name.name, address: address)
- sleep 1
-end
-
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-Product.destroy_all
-50.times do |i|
-	Product.create(
-		name: Faker::Commerce.product_name,
-		price: Faker::Commerce. price.to_i,
-		photo: "https://picsum.photos/400/200?image=#{i + 1}",
-		description: "Loreem ipsum dolor  blablaba"
-		)
-end
-Order.destroy_all
 15.times do |i|
 	Order.create(
+		user_id: 15 + i,
+		product_id: 100 + i,
+		price: product.price,
+		quantity: 1,
+		payed: false
 		)
 end
+
+5.times do |i|
+	Order.create(
+		user_id: 15 + i,
+		product_id: 100 + i,
+		price: product.price,
+		quantity: 1,
+		payed: true
+		)
+end
+
+
+
